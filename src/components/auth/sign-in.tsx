f6558bd3f675c5c50c14126dbd7646dcb8fa7bf1
David Wahrenburg <mail@davidwahrenburg.de>
2025-03-19 13:23:44 +0100

    simlify admin cred

--- src/components/auth/sign-in.tsx
+++ src/components/auth/sign-in.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import {useActionState} from 'react';
-import {authenticate} from "@/lib/actions";
+import { useActionState } from 'react';
+import { authenticate } from "@/lib/actions";
 
 export default function LoginForm() {
     const [errorMessage, formAction, isPending] = useActionState(
@@ -13,23 +13,19 @@ export default function LoginForm() {
         <form action={formAction}>
             <div>
                 <div>
-                    <label htmlFor="email">
-                        Email
-                    </label>
+                    <label htmlFor="username">Username</label>
                     <div className="relative">
                         <input
-                            id="email"
-                            type="email"
-                            name="email"
-                            placeholder="Enter your email address"
+                            id="username"
+                            type="text"
+                            name="username"
+                            placeholder="Enter your username"
                             required
                         />
                     </div>
                 </div>
                 <div>
-                    <label htmlFor="password">
-                        Password
-                    </label>
+                    <label htmlFor="password">Password</label>
                     <div className="relative">
                         <input
                             id="password"
@@ -42,18 +38,9 @@ export default function LoginForm() {
                     </div>
                 </div>
             </div>
-            <button type={"submit"} aria-disabled={isPending}>
-                Log in
-            </button>
-            <div
-                aria-live="polite"
-                aria-atomic="true"
-            >
-                {errorMessage && (
-                    <>
-                        <p className="text-sm text-red-500">{errorMessage}</p>
-                    </>
-                )}
+            <button type="submit" aria-disabled={isPending}>Log in</button>
+            <div aria-live="polite" aria-atomic="true">
+                {errorMessage && <p className="text-sm text-red-500">{errorMessage}</p>}
             </div>
         </form>
     );
